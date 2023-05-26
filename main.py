83e9ce53a34498dfb1b0dc6114cfce904201a850
Burkard920 Burkard <stevenburkard24@yahoo.com>
2023-05-26 15:11:32 -0500

    Created weapon.py file and Weapon class and added functions to robot.py so the Robot is now equipped with a weapon

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from dinosaur import Dinosaur
-from robot import Robot        
+from robot import Robot
+from weapon import Weapon        
 
 
-
+weapon = Weapon("Plasma Pistol", 25)
 dinosaur = Dinosaur("Spinosaurus", 100, 20)
-robot = Robot("Droid", 100, 15)
+robot = Robot("Droid", 100, weapon)
+robot.equip_weapon(weapon)
 
-print(f"{dinosaur.name}")
-print(f"Robot Attack Power: {robot.active_weapon}")
+print(f"{dinosaur.name} has {dinosaur.health} health and {dinosaur.attack_power} attack power")
+
+print(f"{robot.name} has a {weapon.name} Health: {robot.health} and {weapon.attack_power} attack power")
--- robot.py
+++ robot.py
@@ -1,5 +1,13 @@
+from weapon import Weapon
+from dinosaur import Dinosaur
+
 class Robot:
     def __init__(self, name, health, active_weapon):
         self.name = name
         self.health = health
-        self.active_weapon = active_weapon
+        self.active_weapon = None
+
+    def equip_weapon(self, weapon):
+        self.active_weapon = weapon
+
+    
