2ba3cbd62cc94ca3c07c3fe01c193b22bf07399e
Burkard920 Burkard <stevenburkard24@yahoo.com>
2023-05-26 13:28:30 -0500

    Created dinosaur.py with a class Dinosaur, and created robot.py with a class Robot

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+from dinosaur import Dinosaur
+from robot import Robot        
+
+
+
+dinosaur = Dinosaur("Spinosaurus", 100, 20)
+robot = Robot("Droid", 100, 15)
+
+print(f"{dinosaur.name}")
+print(f"Robot Attack Power: {robot.active_weapon}")
